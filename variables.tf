a9357e81a9c2f9d7d0b7c38dad9c0e051f94cf02
greenfieldz <104225864+greenfieldz@users.noreply.github.com>
2022-05-19 21:55:57 -0500

    Variables + tflint

--- variables.tf
+++ variables.tf
@@ -1,1 +1,9 @@
-variable "name" {}
+variable "name" {
+  description = "Name of the IAM role"
+}
+
+# Information about claims can be found here
+# https://docs.github.com/en/actions/deployment/security-hardening-your-deployments/configuring-openid-connect-in-amazon-web-services
+variable "custom_claim" {
+  description = "Github Custom Claims"
+}
